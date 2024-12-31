2fda92d1a8b78097a32b73c3a90fdf80d02087f3
darkars33 <darshangarad18@gmail.com>
2024-12-31 13:10:59 +0530

    add vercel link

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = 3000;
 const allowedOrigins = [
           "http://localhost:5173",
           "http://localhost:3000",
-          "https://example.com",
+          "https://peakline-frontend.vercel.app",
 ];
 app.use(express.json());
 app.use(cookieParser());
